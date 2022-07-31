4ed01165649e202e7079f94488286775dc39096f
Alexander Babak <bbkshow1987@gmail.com>
2022-07-31 20:38:14 +0300

    add logout

--- src/common/components/MainLayout/Header.tsx
+++ src/common/components/MainLayout/Header.tsx
@@ -1,16 +1,19 @@
 import { Layout, Button, Typography } from "antd";
 import styled from "styled-components";
-import { useAppSelector } from "../../../core/redux/reduxType";
+import { useAppDispatch, useAppSelector } from "../../../core/redux/reduxType";
 import { BreadcrumbComponent } from "./BreadcrumbComponent";
 import { useLocation } from "react-router-dom";
+import { logout } from "../../../modules/auth/authSlice";
+import { useCallback } from "react";
 
 const { Header } = Layout;
 const { Title, Paragraph } = Typography;
 
 export const HeaderMenu = () => {
+  const dispatch = useAppDispatch();
   const location = useLocation();
   const { user } = useAppSelector((state) => state.auth);
-
+  const exit = useCallback(() => dispatch(logout()), []);
   const path = location.pathname.split("/");
   const lastElem =
     path[path.length - 1].charAt(0).toUpperCase() +
@@ -29,7 +32,7 @@ export const HeaderMenu = () => {
           <Paragraph>
             {user?.name} {user?.lastname}
           </Paragraph>
-          <Button>Выйти</Button>
+          <Button onClick={exit}>Выйти</Button>
         </User>
       </Flex>
     </Header>
@@ -45,6 +48,7 @@ const Flex = styled.div`
 
 const User = styled(Flex)`
   gap: 15px;
+
   & > div {
     margin: 0;
     line-height: 20px;
