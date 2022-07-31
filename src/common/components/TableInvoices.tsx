dc0b591bd1875c698572c362d5b4f681298df8ed
Alexander Babak <bbkshow1987@gmail.com>
2022-07-31 20:33:04 +0300

    templates empty

---
+++ src/common/components/TableInvoices.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { Table } from "antd";
+import { columns } from "../hooks/invoicesTable";
+import { InvoicesFromServer } from "../../api/invoices/invoicesDto";
+
+interface IProps {
+  invoices: InvoicesFromServer[];
+}
+
+export const TableInvoices: FC<IProps> = ({ invoices }) => {
+  return (
+    <Table
+      columns={columns}
+      dataSource={invoices}
+      rowKey="id"
+      pagination={{ pageSize: 10 }}
+    />
+  );
+};
