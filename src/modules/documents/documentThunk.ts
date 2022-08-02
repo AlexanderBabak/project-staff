61200f621d2c6accc4b259b5725971c9ba1b8b5d
Alexander Babak <bbkshow1987@gmail.com>
2022-08-02 16:13:03 +0300

    add draft

--- src/modules/documents/documentThunk.ts
+++ src/modules/documents/documentThunk.ts
@@ -1,36 +1,56 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-
 import {
   IInvoices,
   InvoicesFromServer,
 } from "../../api/documents/invoices/invoicesDto";
-
 import {
   addInvoicesService,
   getAllInvoicesService,
 } from "../../api/documents/invoices/invoicesService";
+import { DraftForm, IDrafts } from "../../api/documents/drafts/draftsDto";
+import {
+  deleteByIDDraftService,
+  getAllDraftsService,
+  getChangeByIDDraftService,
+} from "../../api/documents/drafts/draftsService";
 
+//invoices
 export const addInvoicesThunk = createAsyncThunk<
   InvoicesFromServer[],
   IInvoices
->(
-  "documents/addInvoices",
+>("documents/addInvoices", async (data) => {
+  const response = await addInvoicesService(data);
+  return response;
+});
 
-  async (data) => {
-    const response = await addInvoicesService(data);
-
+export const getAllInvoicesThunk = createAsyncThunk<InvoicesFromServer[]>(
+  "documents/getAllInvoices",
+  async () => {
+    const response = await getAllInvoicesService();
     return response;
   }
 );
 
-export const getAllInvoicesThunk = createAsyncThunk<InvoicesFromServer[]>(
-  "documents/getAllInvoices",
-
+//drafts
+export const getAllDraftsThunk = createAsyncThunk<IDrafts[]>(
+  "documents/getAllDrafts",
   async () => {
-    const response = await getAllInvoicesService();
-
-    localStorage.setItem("test", JSON.stringify(response[0]));
-
+    const response = await getAllDraftsService();
     return response;
+  }
+);
+
+export const changeDraftThunk = createAsyncThunk<
+  IDrafts,
+  { id: number; data: DraftForm }
+>("documents/changeDraft", async ({ id, data }) => {
+  const response = await getChangeByIDDraftService(id, data);
+  return response;
+});
+
+export const deleteDraftThunk = createAsyncThunk<void, number>(
+  "documents/deleteDraft",
+  async (id) => {
+    await deleteByIDDraftService(id);
   }
 );
